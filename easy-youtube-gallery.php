21a1d104958ca3a7da407a07de816f0fbc89b7d4
Aleksandar Urosevic <urke.kg@gmail.com>
2025-01-16 22:19:15 +0100

    Unify plugin header

--- easy-youtube-gallery.php
+++ easy-youtube-gallery.php
@@ -1,14 +1,13 @@
 <?php
-/*
-Plugin Name: Easy YouTube Gallery
-Plugin URI: https://urosevic.net/wordpress/plugins/easy-youtube-gallery/
-Description: Quick and easy embed thumbnails gallery for custom set of YouTube videos provided in shortcode, and autoplay video on click in Magnific PopUp lightbox.
-Author: Aleksandar Urošević
-Version: 1.0.5
-Author URI: https://urosevic.net/
-Text Domain: easy-youtube-gallery
-*/
-
+/**
+ * Plugin Name: Easy YouTube Gallery
+ * Plugin URI: https://urosevic.net/wordpress/plugins/easy-youtube-gallery/
+ * Description: Quick and easy embed thumbnails gallery for custom set of YouTube videos provided in shortcode, and autoplay video on click in Magnific PopUp lightbox.
+ * Author: Aleksandar Urošević
+ * Version: 1.0.5
+ * Author URI: https://urosevic.net/
+ * Text Domain: easy-youtube-gallery
+ */
 namespace Techwebux\Eytg;
 
 // If this file is called directly, abort.
